6fa4a1adb3591c23845205067a287f4e86074532
din <dina.vintr@gmail.com>
2025-03-07 22:29:35 +0200

    bun

--- apps/bun/server/renderer.ts
+++ apps/bun/server/renderer.ts
@@ -1,8 +1,15 @@
+ 
 import {defineRenderHandler} from "nitropack/runtime";
 
-export default defineRenderHandler((_event) => {
+export default defineRenderHandler(async (_event) => {
+    
+    // @ts-ignore
+    await import("atomico/ssr/load");
+    const {html} = await import("atomico/html");
+    await import('@y-block/cm');
+
     return {
-        body: /* html */ ` `,
+        body:html`<y-cm>server/routes/index.ts</y-cm>`.render(),
     };
 });
 
